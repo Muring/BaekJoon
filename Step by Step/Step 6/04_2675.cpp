26ad08545f5dd966021ba8267212c5bf24fe440b
Muring <11esh@naver.com>
2022-09-22 11:21:59 +0900

    String Loop

---
+++ Step by Step/Step 6/04_2675.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int T, R;
+    string S;
+
+    cin >> T;
+    for(int i = 0; i < T; i ++){
+        cin >> R >> S;
+        for(int j = 0; j < S.length(); j++){
+            for(int s = 0; s < R; s++)
+                cout << S[j];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
